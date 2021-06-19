7be8febc1d62da1b74a9be5d92d45fb4d3cb7bbd
neerajkr25 <69948755+neerajkr25@users.noreply.github.com>
2021-06-19 12:18:51 +0530

    fixed the bug

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world----- Neeraj is your testing manager !! Hi or bye is the string needed here.., ";
+    return "Hello world----- Neeraj is your testing manager !! Hi or Bye is the string needed here.., ";
   }
 }
